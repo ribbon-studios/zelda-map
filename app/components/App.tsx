59fd84aae8bd26d73df93daab28f29b3fdd82422
Cecilia Sanare <admin@cecilias.me>
2023-08-15 22:37:52 -0500

    chore: added fork me button

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import { ScreenModalContent } from './modal/ScreenModalContent';
 import { Header } from './Header';
+import { ForkMe } from '@rain-cafe/fork-me';
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
         <ScreenModalContent map={Maps.QUEST1} />
         <Map map={Maps.QUEST1} />
       </div>
+      <ForkMe slug="rain-cafe/zelda-map" backgroundColor="white" color="#272727" />
     </Provider>
   );
 }
