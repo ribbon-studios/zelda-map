fcb9478a15b6ae958fb1de20bf180641a35c3146
Cecilia Sanare <admin@cecilias.me>
2023-08-18 22:56:11 -0500

    chore: minor adjustments to sha output

--- app/setup.tsx
+++ app/setup.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import dedent from 'dedent';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
@@ -9,4 +10,10 @@ createRoot(document.getElementById('root') as HTMLElement).render(
   </Provider>
 );
 
-console.log(`You're currently running v${process.env.GITHUB_SHA || 'local'}!`);
+const sha = process.env.GITHUB_SHA || 'main';
+if (sha) {
+  console.log(dedent`
+    Version: ${sha}
+    URL: https://github.com/rain-cafe/zelda-map/tree/${sha}
+  `);
+}
